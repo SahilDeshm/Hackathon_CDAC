ae1b8bd7958b9e88c6fed3070bc89d3eb9e40653
shubhammowade <shubhammowade8@gmail.com>
2025-11-14 14:17:03 +0530

    client till nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router ,  Routes , Route   } from "react-router-dom";
-import Login from "./pages/login";
 import Home from "./pages/Home";
-import Register from "./pages/Register";
-
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login1";
+import ShiftingContactForm from "./pages/Create_Review";
+import FlipNavWrapper from "./Navbar/Navbar";
 function App() {
      return(
       <Router>
         <Routes>
-           <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login/>} />
+          <Route path="/create_review" element={<ShiftingContactForm />}/>
           <Route path = "/register" element={<Register/>} /> 
+          <Route path = "/home" element={<FlipNavWrapper/>} /> 
         </Routes>
       </Router>
      )

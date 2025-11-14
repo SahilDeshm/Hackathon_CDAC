cd76fe39dbf55c76899442c0a164faacd179255c
Sahil Deshmukh <101626817+SahilDeshm@users.noreply.github.com>
2025-11-14 09:20:11 +0530

    Setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router ,  Routes , Route   } from "react-router-dom";
+import Login from "./pages/login";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+
+function App() {
+     return(
+      <Router>
+        <Routes>
+           <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path = "/register" element={<Register/>} /> 
+        </Routes>
+      </Router>
+     )
+}
+
+export default App
